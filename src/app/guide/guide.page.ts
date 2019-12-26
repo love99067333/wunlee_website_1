f58375998f3e0286c7643f3640f48317fcdadfb2
love99067333 <48167502+love99067333@users.noreply.github.com>
2019-12-26 17:25:19 +0800

    update
    guide:img slider
    event:form

--- src/app/guide/guide.page.ts
+++ src/app/guide/guide.page.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GuidePage implements OnInit {
 
+  slidesItems: any = [
+    { img: '../../assets/pic/guide/1208-1.jpg' },
+    { img: '../../assets/pic/guide/1208-2.jpg' },
+    { img: '../../assets/pic/guide/1208-3.jpg' },
+    { img: '../../assets/pic/guide/1208-4.jpg' },
+    { img: '../../assets/pic/guide/1208-5.jpg' },
+    { img: '../../assets/pic/guide/1208-6.jpg' },
+    { img: '../../assets/pic/guide/1208-7.jpg' },
+    { img: '../../assets/pic/guide/1208-8.jpg' },
+    { img: '../../assets/pic/guide/1208-9.jpg' },
+    { img: '../../assets/pic/guide/1208-10.jpg' },
+    { img: '../../assets/pic/guide/1208-11.jpg' },
+    { img: '../../assets/pic/guide/1208-12.jpg' },
+    { img: '../../assets/pic/guide/1208-13.jpg' },
+    { img: '../../assets/pic/guide/1208-14.jpg' },
+    { img: '../../assets/pic/guide/1208-15.jpg' },
+    { img: '../../assets/pic/guide/1208-16.jpg' },
+    { img: '../../assets/pic/guide/1208-17.jpg' },
+    { img: '../../assets/pic/guide/1208-18.jpg' }
+  ];
+  slideOpts = {
+    initialSlide: 1,
+    speed: 3000,
+    loop: true,
+    autoplay: true,
+    pager: true
+  };
   constructor() { }
 
   ngOnInit() {
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  
 
   constructor(private route: Router) { }
 
